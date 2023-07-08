5994f5fa423cef22757f78b755972bd0126cbe45
Ilia Pavlov <pavlov.ru90@gmail.com>
2023-07-08 15:33:21 +0300

    test

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log('test')
+import { promises } from 'fs'
+
+import // join,
+// basename,
+// dirname,
+// extname,
+// relative,
+// isAbsolute,
+// resolve,
+// sep,
+'path'
+
+const pathCopy = process.env.PWD
+console.log("pathCopy", pathCopy);
+
+// promises.copyFile('./', pathCopy)
