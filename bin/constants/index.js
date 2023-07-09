2a798c5d6bb51275c5accc794281ec99b544943f
Ilia Pavlov <pavlov.ru90@gmail.com>
2023-07-09 17:05:12 +0300

    add worked npx

---
+++ bin/constants/index.js
@@ -0,0 +1,5 @@
+const directoriesNotBeCopied = ['bin', '.git'];
+const filesNotBeCopied = ['.npmignore'];
+const emptySettingsJson = ['name', 'version', 'keywords', 'author', 'description'];
+
+module.exports = { directoriesNotBeCopied, filesNotBeCopied, emptySettingsJson };
---
+++ bin/utils/copyDirectory.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyDirectory(sourceDir, targetDir, directoriesNotBeCopied = [], filesNotBeCopied = []) {
+  // create directory
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir);
+  }
+
+  // get list files and directories in root directory
+  const files = fs.readdirSync(sourceDir);
+
+  // copy files and directories
+  files.forEach(file => {
+    const sourcePath = path.join(sourceDir, file);
+    const targetPath = path.join(targetDir, file);
+
+    if (fs.statSync(sourcePath).isDirectory()) {
+      // don't copy some directories
+      if (!directoriesNotBeCopied.includes(file)) copyDirectory(sourcePath, targetPath);
+    } else {
+      // don't copy some files
+      if (!filesNotBeCopied.includes(file)) fs.copyFileSync(sourcePath, targetPath);
+    }
+  });
+}
+
+module.exports = { copyDirectory };
