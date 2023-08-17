bf84b3eda3ea8fbc8e95ed4e5bade79d5e6ab0c6
Ilia Pavlov <pavlov.ru90@gmail.com>
2023-08-17 12:32:42 +0300

    example clsx

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -1,6 +1,21 @@
 import clsx from 'clsx'
 import styles from './Hello.module.scss'
+import React from 'react'
 
 export const Hello = () => {
-  return <h1 className={clsx(styles.root, styles.test)}>Hello React!</h1>
+  const [state, setState] = React.useState<boolean>(false)
+
+  return (
+    <>
+      <div
+        className={clsx(styles.root, { [styles.test]: state })}
+        onClick={() => setState((prev) => !prev)}
+        onKeyDown={() => setState((prev) => !prev)}
+        role="button"
+        tabIndex={0}
+      >
+        Hello React!
+      </div>
+    </>
+  )
 }
